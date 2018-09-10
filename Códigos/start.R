1b8586410d10695a5ee75449fdf8c82a0ec72705
RubenRenovell <43041224+RubenRenovell@users.noreply.github.com>
2018-09-10 18:08:46 +0200

    Base de datos mensualizada

--- Códigos/start.R
+++ Códigos/start.R
@@ -5,6 +5,10 @@
 #PAQUETES
 #install.packages("dplyr")
 library(dplyr)
+#install.packages("bigmemory")
+library(bigmemory)
+#install.packages("translateR")
+#library(translateR)
 
 #LA SIGUIENTE SENTENCIA ES PARA VER BIEN LOS CARÁCTERES EN RUSO, HAY
 #QUE ACOMPAÑARLO CON CON EL ENCODING="UTF-8"
@@ -16,6 +20,18 @@ test=read.csv("C:/Users/Ruben/Desktop/Ruben/Kaggle/Future Sales/test.csv")
 shops=read.csv("C:/Users/Ruben/Desktop/Ruben/Kaggle/Future Sales/shops.csv",encoding="UTF-8")
 items=read.csv("C:/Users/Ruben/Desktop/Ruben/Kaggle/Future Sales/items.csv",encoding="UTF-8")
 cat_items=read.csv("C:/Users/Ruben/Desktop/Ruben/Kaggle/Future Sales/item_categories.csv",encoding="UTF-8")
+
+#AGRUPAMOS LOS DATOS EN MESES
+datos=select(datos,-date)
+datos=datos%>%
+  group_by(date_block_num,shop_id,item_id)%>%
+  summarise(sum_precio=sum(item_price),ventas=sum(item_cnt_day))
+datos$precio_medio=datos$sum_precio/datos$ventas
+
+#CREAMOS EL DATO DEL SIGUIENTE MES
+test$date_block_num=34
+
+#google.dataset.out <- translate(dataset = shops,content.field = 'shop_name',google.api.key = my.api.key,source.lang = 'ru',target.lang = 'es')
 
 #UNIMOS EN NUESTROS DATOS TODAS LAS VARIABLES
 datos = datos%>%left_join(shops,by="shop_id")
